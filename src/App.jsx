1158c614099c2db8b590a040675e4e2c8c1732f2
RCAbney <ryan@rcabney.com>
2023-12-05 21:12:57 -0500

    refactor dat form a bit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import * as Form from "@radix-ui/react-form";
-import { Formik } from "formik";
+// import * as Form from "@radix-ui/react-form";
+import { Formik, Field, Form } from "formik";
 import { QuestionFormSchema } from "./validationSchemas/questionFormSchema";
 import "./App.css";
 
@@ -7,7 +7,12 @@ function App() {
     return (
         <div className="h-screen flex items-center justify-center">
             <Formik
-                initialValues={{ email: "", question: "", preference: "" }}
+                initialValues={{
+                    email: "",
+                    choice: "",
+                    preference: "",
+                    question: "",
+                }}
                 validationSchema={QuestionFormSchema}
                 onSubmit={(values, { setSubmitting }) => {
                     setTimeout(() => {
@@ -18,95 +23,172 @@ function App() {
                 }}
             >
                 {({
-                    values,
                     errors,
                     touched,
-                    handleChange,
-                    handleBlur,
                     handleSubmit,
                     isSubmitting,
                 }) => (
-                    <Form.Root
+                    <Form
                         onSubmit={handleSubmit}
                         className="w-96 p-6 border border-gray-300 rounded-md"
                     >
-                        <Form.Field name="email" className="mb-6">
+                        <div name="email" className="mb-6">
                             <div>
-                                <Form.Label className="block text-sm font-medium leading-6 text-gray-900">
+                                <label className="block text-sm font-medium leading-6 text-gray-900">
                                     Email
-                                </Form.Label>
+                                </label>
                             </div>
                             {errors.email && touched.email && (
-                                <Form.Message className="mt-2 text-sm text-red-600">
+                                <div className="mt-2 text-sm text-red-600">
                                     {errors.email}
-                                </Form.Message>
+                                </div>
                             )}
-                            <Form.Control asChild className="mt-2">
-                                <input
+                            <div className="mt-2">
+                                <Field
                                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                     type="email"
-                                    placeholder="example@email.com"
-                                    required
-                                    value={values.email}
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
+                                    name="email"
+                                    placeholder="email address"
                                 />
-                            </Form.Control>
-                        </Form.Field>
-                        <Form.Field name="preference" className="mb-6">
+                            </div>
+                        </div>
+                        <div className="mb-6">
+                            <fieldset>
+                                <div>
+                                    <label className="block text-sm font-medium leading-6 text-gray-900">
+                                        Choose something
+                                    </label>
+                                </div>
+                                {errors.choice && touched.choice && (
+                                    <div className="mt-2 text-sm text-red-600">
+                                        {errors.choice}
+                                    </div>
+                                )}
+                                <div className="space-y-5">
+                                    <div className="relative flex items-start">
+                                        <div className="flex h-6 items-center">
+                                            <Field
+                                                type="radio"
+                                                name="choice"
+                                                value="yes"
+                                                className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                                            />
+                                        </div>
+                                        <div className="ml-3 text-sm leading-6">
+                                            <label
+                                                htmlFor="yes"
+                                                className="font-medium text-gray-900"
+                                            >
+                                                Yes
+                                            </label>{" "}
+                                            <span className="text-gray-500">
+                                                alright chums let&rsquo;s do
+                                                this
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="space-y-5">
+                                    <div className="relative flex items-start">
+                                        <div className="flex h-6 items-center">
+                                            <Field
+                                                type="radio"
+                                                name="choice"
+                                                value="no"
+                                                className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                                            />
+                                        </div>
+                                        <div className="ml-3 text-sm leading-6">
+                                            <label
+                                                htmlFor="no"
+                                                className="font-medium text-gray-900"
+                                            >
+                                                No
+                                            </label>{" "}
+                                            <span className="text-gray-500">
+                                                Not on ur life pal
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="space-y-5">
+                                    <div className="relative flex items-start">
+                                        <div className="flex h-6 items-center">
+                                            <Field
+                                                type="radio"
+                                                name="choice"
+                                                value="idk"
+                                                className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                                            />
+                                        </div>
+                                        <div className="ml-3 text-sm leading-6">
+                                            <label
+                                                htmlFor="iDontKnow"
+                                                className="font-medium text-gray-900"
+                                            >
+                                                I don&rsquo;t know
+                                            </label>{" "}
+                                            <span className="text-gray-500">
+                                                Hello there.
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </fieldset>
+                        </div>
+                        <div className="mb-6">
                             <div>
-                                <Form.Label className="block text-sm font-medium leading-6 text-gray-900">
+                                <label className="block text-sm font-medium leading-6 text-gray-900">
                                     Preference
-                                </Form.Label>
+                                </label>
                                 {errors.preference && touched.preference && (
-                                    <Form.Message className="mt-2 text-sm text-red-600">
+                                    <div className="mt-2 text-sm text-red-600">
                                         {errors.preference}
-                                    </Form.Message>
+                                    </div>
                                 )}
                             </div>
-                            <Form.Control asChild className="mt-2">
-                                <select
+                            <div className="mt-2">
+                                <Field
+                                    as="select"
+
+                                    name="preference"
                                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                    value={values.preference}
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
                                 >
                                     <option value="">---</option>
-                                    <option value="yes">yes</option>
-                                    <option value="no">no</option>
-                                </select>
-                            </Form.Control>
-                        </Form.Field>
-                        <Form.Field name="question" className="mb-6">
+                                    <option value="up">up</option>
+                                    <option value="down">down</option>
+                                </Field>
+                            </div>
+                        </div>
+                        <div name="question" className="mb-6">
                             <div>
-                                <Form.Label className="block text-sm font-medium leading-6 text-gray-900">
+                                <label className="block text-sm font-medium leading-6 text-gray-900">
                                     Question
-                                </Form.Label>
+                                </label>
                                 {errors.question && touched.question && (
-                                    <Form.Message className="mt-2 text-sm text-red-600">
+                                    <div className="mt-2 text-sm text-red-600">
                                         {errors.question}
-                                    </Form.Message>
+                                    </div>
                                 )}
                             </div>
-                            <Form.Control asChild className="mt-2">
-                                <textarea
+                            <div className="mt-2">
+                                <Field
+                                    as="textarea"
                                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                    required
-                                    value={values.question}
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
+                                    name="question"
                                 />
-                            </Form.Control>
-                        </Form.Field>
-                        <Form.Submit asChild>
+                            </div>
+                        </div>
+                        <div>
                             <button
+                                type="submit"
                                 disabled={isSubmitting}
                                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                             >
                                 Post question
                             </button>
-                        </Form.Submit>
-                    </Form.Root>
+                        </div>
+                    </Form>
                 )}
             </Formik>
         </div>
--- src/validationSchemas/questionFormSchema.js
+++ src/validationSchemas/questionFormSchema.js
@@ -2,6 +2,7 @@ import * as Yup from 'yup'
 
 export const QuestionFormSchema = Yup.object().shape({
     email: Yup.string().email("Not a valid email").required('You must provide an email address'),
+    choice: Yup.string().required('you must choose wisely'),
     preference: Yup.string().required("Please select a preference"),
-    question: Yup.string().required('Please ask a question').max(150, "That's too much!")
+    question: Yup.string().required('Please ask a question').max(150, "That's too much!"),
 })
